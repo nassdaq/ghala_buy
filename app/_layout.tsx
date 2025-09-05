17c3f6ea692683c9c81ec111cae79746ea4f38a4
nassdaq <mwaijegakelvin@gmail.com>
2025-09-05 14:44:56 +0300

    feat(auth): implement authentication check and update splash screen color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,24 +6,59 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const token = await AsyncStorage.getItem('user_token');
+        console.log('Token retrieved:', token);
+        setIsAuthenticated(token !== null && token.trim() !== '');
+      } catch (error) {
+        console.error('Error retrieving token:', error);
+        setIsAuthenticated(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, []);
+
+  if (!loaded || loading) {
+    // Async font loading only occurs in development or auth check in progress.
     return null;
   }
 
+  console.log('Is authenticated:', isAuthenticated);
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(app)/(tabs)" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }} >
+        {isAuthenticated ? (
+          <Stack.Screen name="(auth)/index" options={{ headerShown: false }} />
+        ) : (
+          <Stack.Screen name="(app)/(tabs)" options={{ headerShown: false }} />
+        )}
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
+/*
+
+
+
+
+ */
